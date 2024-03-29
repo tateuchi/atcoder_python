e23720fe11d7fd6b285ee2b7f1ce226b33728cec
tateuchi <tatte_himmel@ruri.waseda.jp>
2024-03-29 17:36:06 +0000

    進捗更新 鉄則A71，72

--- practice/tessoku-book/a71/main.py
+++ practice/tessoku-book/a71/main.py
@@ -1,12 +1,19 @@
 import sys
-def i():
-    return int(sys.stdin.readline())
-def mi():
-    return map(int, sys.stdin.readline().split())
-def li():
-    return list(map(int, sys.stdin.readline().split()))
-def s():
-    return sys.stdin.readline().strip()
-def ls(n):
-    return [list(sys.stdin.readline()) for _ in range(n)]
+ri = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(ri())
+mi = lambda: map(int, ri().split())
+li = lambda: list(map(int, ri().split()))
+mia = lambda *t: [f(ri()) for f in t]
+bi = lambda n: [li() for _ in range(n)]
 
+N=ii()
+A=li()
+B=li()
+
+A.sort()
+B.sort(reverse=True)
+ans=0
+for i in range(N):
+  ans += A[i]*B[i]
+
+print(ans)
